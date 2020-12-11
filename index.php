55c925f99ec33847e4aee8c9b64da743f39e065f
Francisco Javier Silva Cadavid <francisco.silva@udea.edu.co>
2020-12-11 12:45:55 -0500

    Se está organizando Index

--- index.php
+++ index.php
@@ -1,27 +1,19 @@
 <?php include("includes/header.php"); ?>
 
-    <header class="page-header font-small text-center pt-4 pb-3" style="background-color:#c79be0; color:white" >
-        <a> Página principal </a>
-    </header>
-
-    <br><br><br><br><br>
-
-    <div class="container p-4 text-center">
-        <form action ="form_official.php"  method="POST">
-            <input type = "submit" name= "nologin" value = "Formulario Funcionario"  style="background-color:#DCDCDC; border-color:black; color:black"  class="btn btn-lg"></input>
-        </form>
-    </div>
-
-    <div class="container p-4 text-center">
-    <form action ="form_contractor.php"  method="POST">
-    <input type = "submit" name= "nologin" value = "formulario Contratista"  style="background-color:#DCDCDC; border-color:black; color:black"  class="btn btn-lg"></input>
+<div class="container p-4 text-center">
+    <form action="form_official.php" method="POST">
+        <input type="submit" name="nologin" value="Formulario Funcionario"
+            style="background-color:#DCDCDC; border-color:black; color:black" class="btn btn-lg"></input>
     </form>
-    </div>
-
-    <div class="container p-4 text-center">
-        <form action ="login.php"  method="POST">
-            <input type = "submit" value = "Ingresar como administrador"  style="background-color:#DCDCDC; border-color:black; color:black"  class="btn btn-lg"></input>
-        </form>
-    </div>
+    <form action="form_contractor.php" method="POST">
+        <input type="submit" name="nologin" value="Formulario Contratista"
+            style="background-color:#DCDCDC; border-color:black; color:black" class="btn btn-lg"></input>
+    </form>
+    <form action="login.php" method="POST">
+        <input type="submit" value="Ingresar como administrador"
+            style="background-color:#DCDCDC; border-color:black; color:black" class="btn btn-lg"></input>
+    </form>
+</div>
+<!-- Hola, Arenas -->
 
 <?php include("includes/footer.php"); ?>
